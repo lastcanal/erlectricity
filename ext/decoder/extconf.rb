f325b7424d5a2292bda80b7e8f6638a43562f5bc
Stephan Renatus <srenatus@chef.io>
2017-06-29 12:10:43 +0200

    extconf: remove stuff travis' GCC complains about
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- ext/decoder/extconf.rb
+++ ext/decoder/extconf.rb
@@ -1,2 +1,8 @@
 require 'mkmf'
+
+CONFIG['warnflags'].slice!(/ -Wdeclaration-after-statement/)
+CONFIG['warnflags'] << ' -Wno-sign-compare'
+CONFIG['warnflags'] << ' -Wno--Wstrict-aliasing'
+$CFLAGS << ' -std=c99'
+
 create_makefile('decoder')
